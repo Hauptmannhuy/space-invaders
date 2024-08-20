abc0825dd2d10b9f74119117d48a6458abfd680c
hauptmannhuy <dindago2324@gmail.com>
2024-08-20 18:40:11 +0300

    added initial foundation for shooting feature

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ running = True
 y,x = 700,800
 width,height = 50,50
 
-start_projectile_y, start_projectile_x = y,x
+end_projectile_y, end_projectile_x = y,x
 fps = pygame.time.Clock()
+
+shot = False
+start_projectile_x = start_projectile_y = end_projectile_y = end_projectile_x = 0
 
 while (running):
   fps.tick(60)
@@ -19,16 +22,29 @@ while (running):
       
   screen.fill('black') 
   keys = pygame.key.get_pressed()
-  if keys[pygame.K_w] == True:
+  if keys[pygame.K_w]:
     y-= 8
-  elif keys[pygame.K_s] == True:
+  elif keys[pygame.K_s]:
     y+= 8
-  elif keys[pygame.K_a] == True:
+  elif keys[pygame.K_a]:
     x-= 8
-  elif keys[pygame.K_d] == True:
+  elif keys[pygame.K_d]:
     x+= 8
-  elif keys[pygame.K_SPACE] == True:
-    pygame.draw.line(screen,'red',(x+width/2,y-10),(x+width/2,y-40),5)
+  
+  if keys[pygame.K_SPACE]:
+    if not shot:
+      start_projectile_x = x+width/2
+      start_projectile_y = y
+      end_projectile_y = start_projectile_y-850
+      shot = True
+  
+  if(shot == True and start_projectile_y > end_projectile_y):
+    start_projectile_y-=20
+    pygame.draw.line(screen,'red',(start_projectile_x,start_projectile_y),(start_projectile_x,start_projectile_y-20),5)
+  else:
+    shot = False
+    
+      
   pygame.draw.rect(screen,'yellow',pygame.Rect(x,y,width,height))
   pygame.display.update()
 
